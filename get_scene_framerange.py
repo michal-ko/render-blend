27b4a0da1f9ee8e23f0ce7c2476883d4795f5462
michal-ko <michal.koziatek@protonmail.com>
2020-08-24 17:25:48 +0100

    improved argument and path handling. Now returns a dict

--- get_scene_framerange.py
+++ get_scene_framerange.py
@@ -2,20 +2,24 @@ import bpy
 
 
 def get_framerange(scenepath):
-    start_frame =  bpy.context.scene.frame_start
-    end_frame =  bpy.context.scene.frame_end
-    return (start_frame, end_frame)
-    
+    bpy.ops.wm.open_mainfile(filepath=scenepath)  # opens blender scene
+    start_frame = bpy.context.scene.frame_start
+    end_frame = bpy.context.scene.frame_end
+    return {"start_frame": start_frame, "end_frame": end_frame}
+
 
 if __name__ == "__main__":
-    
-    import sys       # to get command line args
-    import argparse  # to parse options for us and print a nice help message
-    
-    if len(sys.argv) != 2:
-        print("Incorrect arguments provided. Use blend scene path only")
+
+    import sys
+
+    if len(sys.argv) != 5:
+        print("Incorrect arguments provided!\n"
+              "Example: blender --background monkey_test.blend --python ./get_scene_framerange.py")
         sys.exit(1)
-    
+
+    abs_file_path = bpy.data.filepath
+    print(f"Checking file: {abs_file_path}")
+
     # Run the function to extract frame range
-    fr = get_framerange(sys.argv[1])
+    fr = get_framerange(abs_file_path)
     print(fr)
